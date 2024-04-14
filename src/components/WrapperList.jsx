a4a8786f9a067292faf75434a4832afea4c340e7
FernandaMar <fernanda.martinez5010@gmail.com>
2024-04-13 23:44:45 -0400

    feature(common): sending first question of  lab07

---
+++ src/components/WrapperList.jsx
@@ -0,0 +1,12 @@
+export const WapperList = props => {
+    const { children } = props 
+
+    return (
+        <div>
+            <h2>Lista de compras</h2>
+            <div>
+                { children }
+            </div>
+        </div>
+    )
+}
